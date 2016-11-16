09e6b28736b4e8fbd83cbb8fd0b5283458e5f81a
MFT <michael.totoraitis@gmail.com>
2016-11-16 16:20:19 -0600

    edits

--- gettingdata.R
+++ gettingdata.R
@@ -1,14 +1,18 @@
-if (!file.exists("data")) {
-  dir.create("data")
-}
+#if (!file.exists("data")) {
+#  dir.create("data")
+#}
+setwd("/Users/.../Documents/data/")
 
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 
-download.file(fileUrl, destfile = "./data/data.csv", method = "curl"
-list.files("./data")
+download.file(fileUrl, destfile = "/Users/.../Documents/data/prop.csv", method = "curl")
+list.files("/Users/.../Documents/data")
 
 dateDownloaded <- date()
+#setwd("~/data")
+ 
+prop <- read.csv(prop.csv)
 
-val <- data$val
-val_sorted <- val[order(,data$val)]
+val <- prop$val
+val_sorted <- val[order(,prop$val)]
 
